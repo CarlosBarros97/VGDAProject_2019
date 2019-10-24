0a5bc94aacba7b4d6f9c18b9bf2fe6ce42244665
Carlos_Barros97 <kadumbarros@gmail.com>
2019-10-23 17:33:29 -0700

    Add Health Collectible script

---
+++ Assets/Standard Assets/2D/Scripts/HealthCollectible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{ public float LifeAdd = 20f;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+     HealthDrain.HealthValue = HealthDrain.HealthValue + 20f;
+     Destroy(gameObject);
+    }
+}
