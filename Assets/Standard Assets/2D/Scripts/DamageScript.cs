224680a146b59556da1319c0f467ac2e09a7b20d
Carlos_Barros97 <kadumbarros@gmail.com>
2019-11-03 16:40:45 -0800

    Added patrol script and "enemy"

--- Assets/Standard Assets/2D/Scripts/DamageScript.cs
+++ Assets/Standard Assets/2D/Scripts/DamageScript.cs
@@ -31,7 +31,6 @@ public class DamageScript : MonoBehaviour
         {
             HealthDrain.HealthValue = HealthDrain.HealthValue - LifeTake;
             Invincible = true;
-
         }
     }
 }
