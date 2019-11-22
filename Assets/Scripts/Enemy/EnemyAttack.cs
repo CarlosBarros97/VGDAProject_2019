c5de946a987777b82748dccfa1c5e94012af0a52
Carlos_Barros97 <kadumbarros@gmail.com>
2019-11-22 11:12:35 -0800

    Added flickering when getting attacked by enemies

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -18,6 +18,11 @@ public class EnemyAttack : MonoBehaviour
     public bool Invincible; //invincibility frames
     public float LifeTake = 20f;
     float InvincibleTimer = 2f;
+    private float FlickeringTimer = 0.1f;
+    private bool SpriteDisabled = false;
+    public GameObject player = GameObject.FindGameObjectWithTag("Player");
+    private SpriteRenderer spriteRenderer;
+    private float EmergencyTimer = 2f;
 
     [SerializeField]
     Animator anim;
@@ -27,6 +32,7 @@ public class EnemyAttack : MonoBehaviour
 
     void Start()
     {
+        spriteRenderer = player.GetComponent<SpriteRenderer>();
         Invincible = false;
     }
     void Awake()
@@ -37,13 +43,25 @@ public class EnemyAttack : MonoBehaviour
 
     private void Update()
     {
+
+        if(spriteRenderer.enabled == false)
+        {
+            EmergencyTimer -= Time.deltaTime;
+            if(EmergencyTimer <= 0)
+            {
+                spriteRenderer.enabled = true;
+            }
+        }
+
         if (Invincible == true)
         {
+            SpriteFlickering();
             InvincibleTimer = InvincibleTimer - Time.deltaTime; //timer counts down
             if (InvincibleTimer <= 0) //stops when reaching 0
             {
                 Invincible = false;
                 InvincibleTimer = 2f;
+                spriteRenderer.enabled = true;
             }
         }
 
@@ -70,5 +88,19 @@ public class EnemyAttack : MonoBehaviour
         }
         //remove if attack animation gets implimented
         // anim.SetBool("Attacking", attacking);
+    }
+    void SpriteFlickering()
+    {
+        spriteRenderer.enabled = false;
+        SpriteDisabled = true;
+        if (SpriteDisabled == true)
+        {
+            FlickeringTimer = FlickeringTimer - Time.deltaTime;
+            if (FlickeringTimer <= 0)
+            {
+                spriteRenderer.enabled = true;
+                FlickeringTimer = 0.1f;
+            }
+        }
     }
 }
